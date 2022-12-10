4739dfa3227802598206d1fdb183e911ca543042
k.santhosh <64588768+santhosh1900@users.noreply.github.com>
2022-12-11 02:54:57 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,103 @@
+const express           = require("express"),
+      serverless        = require("serverless-http"), 
+      mongoose          = require("mongoose"),
+      PORT              = process.env.PORT || 3000;
+      app               = express(),
+      {MONGOURI}        = require("../key"),
+      bodyParser        = require("body-parser"),
+      cookieParser      = require("cookie-parser"),
+      route             = require("../routes/vegitable"),
+      user_route        = require("../routes/User"),
+      server_route      = require("../routes/ServerSide"),
+      ad_route          = require("../routes/Advertisement"),
+      Comment_route     = require("../routes/Comment"),
+      Message_route     = require("../routes/Message"),
+      logger            = require("morgan"),
+      cors              = require("cors"),
+      _                 = require("lodash"),
+      router            = express.Router();
+
+
+//io config
+app.use(cors());
+
+const server            = require("http").createServer(app);
+
+const io = require("socket.io")(server, {
+    cors: {
+      credentials: true,
+      methods: ["GET"],
+      origin: '*',
+      optionsSuccessStatus: 200,
+    },
+    allowEIO3: true,
+});
+      
+
+app.use(cookieParser());
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+const { User }  = require("../Helpers/UserClass");
+
+app.use(express.json({ limit : "50mb" }));
+
+app.use(express.urlencoded({ extended : true  , limit : "50mb"}));
+
+// app.use(express.static(__dirname+"/public"));
+
+
+
+mongoose.Promise        = global.Promise;
+mongoose.connect(MONGOURI,{
+    useNewUrlParser: true,
+    useCreateIndex : true,
+    useUnifiedTopology: true,
+    useFindAndModify : false	
+}).then(() =>{
+    console.log("connected to db cluster");
+}).catch(err =>{
+    console.log("error",err.message);
+});  
+
+router.get("/" , (req,res) => {
+    res.json({message : "Nothing to view here"});
+})
+
+
+
+
+io.on("connection" , socket=>{
+    socket.on("create_room" , (data) => {
+        socket.join(data.room);
+    });
+
+    socket.on("start_typing" , data=>{
+        io.to(data.receivername).emit("is_typing",data);
+    });
+
+    socket.on("stop_typing" , data=>{
+        io.to(data.receivername).emit("has_stopped_typing",data);
+    });
+
+    socket.on("send_new_message" , data=>{
+        io.to(data.receiverId).emit("message_received",data);
+    });
+});
+
+app.use('/.netlify/functions/api',router); 
+app.use('/.netlify/functions/api',route); 
+app.use('/.netlify/functions/api',user_route);
+app.use('/.netlify/functions/api',server_route);
+app.use('/.netlify/functions/api',ad_route);
+app.use('/.netlify/functions/api',Comment_route);
+app.use('/.netlify/functions/api',Message_route);
+
+
+module.exports = app;
+module.exports.handler = serverless(app);
+
+
+// server.listen(PORT,()=>{
+//     console.log("server is running in port" ,PORT);
+// });
